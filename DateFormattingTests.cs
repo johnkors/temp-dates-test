441aeefcc1d4badc9470ab4007f60883f029c34b
John Korsnes <johnkors@gmail.com>
2020-06-29 15:42:18 +0200

    Naming of tests

--- DateFormattingTests.cs
+++ DateFormattingTests.cs
@@ -15,40 +15,39 @@ namespace temp_dates_test
         }
         
         [Fact]
-        public void OutputsCorrectly_FromDateTimeOffset1()
+        public void OutputsCorrectly_FromDateTimeOffset_ConvertedWithConvertTime()
         {
             var leet = new DateTimeOffset(2020,6,29,13,37,10,new TimeSpan(0,2,0,0));
-
-            var timezoneId = !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Europe/Oslo" : "Central European Standard Time";
-            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
+            var norwegian = TimeZoneInfo.ConvertTime(leet, GetNorwegianTimeZoneInfo());
             
-            var norwegian = TimeZoneInfo.ConvertTime(leet, timeZoneInfo);
             Assert.Equal("2020-06-29 13:37:10+02:00", norwegian.ToString("yyyy-MM-dd HH:mm:sszzz"));
         }
-        
+
         [Fact]
-        public void OutputsCorrectly_FromDateTimeOffset2()
+        public void OutputsCorrectly_FromDateTimeOffset_Directly()
         {
-            var timezoneId = !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Europe/Oslo" : "Central European Standard Time";
-            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
-            
             var date = new DateTime(2020,6,29, 13,37,10);
-            var offset = timeZoneInfo.GetUtcOffset(date);
+            var offset = GetNorwegianTimeZoneInfo().GetUtcOffset(date);
             var dateWithOffset = new DateTimeOffset(date,offset);
 
             Assert.Contains("2020-06-29 13:37:10+02:00", dateWithOffset.ToString("yyyy-MM-dd HH:mm:sszzz"));
         }
-        
+
         [Fact]
-        public void OutputsCorrectly_FromDateTimeDate()
+        public void OutputsCorrectly_FromDateTime_ConvertedWithConvertTime()
         {
-            var timezoneId = !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Europe/Oslo" : "Central European Standard Time";
-            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
-            
             var date = new DateTime(2020,6,29, 13,37,10);
-            var converted = TimeZoneInfo.ConvertTime(date, timeZoneInfo);
+            var converted = TimeZoneInfo.ConvertTime(date, GetNorwegianTimeZoneInfo());
 
             Assert.Equal("2020-06-29 13:37:10+02:00", converted.ToString("yyyy-MM-dd HH:mm:sszzz"));
+        }
+
+        private static TimeZoneInfo GetNorwegianTimeZoneInfo()
+        {
+            var timezoneId = !RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+                ? "Europe/Oslo"
+                : "Central European Standard Time";
+            return TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
         }
     }
 }
