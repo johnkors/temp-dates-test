5184a46d22d77977cfba1566851d1ad6c89f686a
John Korsnes <johnkors@gmail.com>
2020-06-29 15:14:15 +0200

    Adds tz test

--- DateFormattingTests.cs
+++ DateFormattingTests.cs
@@ -1,11 +1,19 @@
 using System;
 using System.Runtime.InteropServices;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace temp_dates_test
 {
     public class DateFormattingTests
     {
+        private readonly ITestOutputHelper _helper;
+
+        public DateFormattingTests(ITestOutputHelper helper)
+        {
+            _helper = helper;
+        }
+        
         [Fact]
         public void OutputsCorrectly()
         {
@@ -13,8 +21,24 @@ namespace temp_dates_test
 
             var timezoneId = !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Europe/Oslo" : "Central European Standard Time";
             var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
+            
             var norwegian = TimeZoneInfo.ConvertTime(leet, timeZoneInfo);
             Assert.Equal("2020-06-29 13:37:10+02:00", norwegian.ToString("yyyy-MM-dd HH:mm:sszzz"));
+        }
+        
+        [Fact]
+        public void OutputsCorrectlyFromUtc()
+        {
+            var utc = DateTimeOffset.UtcNow;
+
+            var timezoneId = !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "Europe/Oslo" : "Central European Standard Time";
+            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timezoneId);
+            
+            var norwegian = TimeZoneInfo.ConvertTime(utc, timeZoneInfo);
+            _helper.WriteLine(utc.ToString("yyyy-MM-dd HH:mm:sszzz"));
+            _helper.WriteLine(norwegian.ToString("yyyy-MM-dd HH:mm:sszzz"));
+
+            Assert.Contains("+02:00", norwegian.ToString("yyyy-MM-dd HH:mm:sszzz"));
         }
     }
 }
